b5020a33b9c90aa4892386ca6496b3fd6d49e607
Igor Rafael <Igorrafael20102010@gmail.com>
2022-02-07 18:58:07 -0300

    Aula 2

--- src/main/java/br/com/springboot/controllers/UserController.java
+++ src/main/java/br/com/springboot/controllers/UserController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.OptionalInt;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,12 +14,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.springboot.model.User;
+import br.com.springboot.repository.UserRepository;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
     
     private List<User> users = new ArrayList<>();
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/{id}")
     public User user(@PathVariable("id") Long id) {
@@ -42,8 +47,9 @@ public class UserController {
 
     @PostMapping("/")
     public User user(@RequestBody User user) {
-        users.add(user);
-        return user;
+        // users.add(user);
+        return this.userRepository.save(user);
+        // return user;
     }
 
     @GetMapping("/List")
